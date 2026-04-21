ec5c6c8ed6341462ff5c383b08982ce203d8e8f0
delios17 <dntt175.san@gmail.com>
2026-04-21 12:37:42 -0500

    prueba de sonido

--- operadores-aritmeticos.py
+++ operadores-aritmeticos.py
@@ -8,7 +8,7 @@ b = 4
 print("Suma:", a + b)         # 13
 print("Resta:", a - b)        # 7
 print("Multiplicación:", a * b) # 30
-print("División real:", a / b)  # 3.3333...
+print("División real:", a / b)  # 3.3333... nomas quiero ver si es q si cambia todo XDD
 print("División entera:", a // b) # 3
 print("Residuo:", a % b)       # 1
 print("Potenciación:", a ** b) # 1000 (10^3)
